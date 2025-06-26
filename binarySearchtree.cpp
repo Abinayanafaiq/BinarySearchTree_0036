1ec31f50e5aadc11971d22026e516257d51c6110
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-06-26 11:18:16 +0700

    belajarbinary

---
+++ binarySearchtree.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public: 
+        int info;
+        Node *leftchild;
+        Node *rightchild;
+
+
+        Node(){
+            leftchild = nullptr;
+            rightchild = nullptr;
+        }
+    };
+
+class BinaryTree{
+    public:
+        Node *ROOT;
+
+    BinaryTree()
+    {
+        ROOT = nullptr;
+    }
+
+    void insert()
+    {
+        int x;
+        cout << " Masukan Nilai: ";
+        cin >> x;
+
+        Node *newNode = new Node();
+
+
+        newNode->info = x;
+
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        Node *parent = nullptr;
+        Node *currentNode = nullptr;
+        search(x, parent, currentNode)
+
+
+        if(parent == nullptr)
+        {
+            ROOT = newMode;
+            return;
+        }
+
+
+        if(x < parent->info)
+        {
+            
+        }
+    }
+};
