a37ea47ca925ecaf88f631a0f01f8239eec7a597
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-06-26 11:48:45 +0700

    int main

--- binarySearchtree.cpp
+++ binarySearchtree.cpp
@@ -70,12 +70,12 @@ class BinaryTree{
         while((currentNode != nullptr) && (currentNode->info !=element))
         {
             parent = currentNode;
-            if(element < currentNode->info);
+            if(element < currentNode->info)
                 currentNode = currentNode->leftchild;
             else
                 currentNode = currentNode->rightchild;
                 
-        };
+        }
     }
 
 
@@ -126,3 +126,55 @@ class BinaryTree{
         return ROOT == nullptr;
     }
 };
+
+
+int main(){
+    BinaryTree x;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            x.insert();
+            break;
+        }
+        case '2':
+        {
+            x.inorder(x.ROOT);
+            break;
+        }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
+        case '4':
+        {
+
+            x.postorder(x.ROOT);
+            break;
+        }
+        case '5':
+            return 0;
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
+}
